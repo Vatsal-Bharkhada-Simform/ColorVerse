817b082e010e71e8befaa3006b4536b35167d9c5
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-05-20 18:31:57 +0530

    Feat: Add infinite scrolling.

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -1,24 +1,50 @@
+import { useEffect, useRef, useState, type UIEventHandler } from "react";
 import ColorBlock from "../components/ColorBlock";
 import { generateHex } from "../utils/generateHex";
 import { useNavigate } from "react-router";
 
 export default function Content(){
     const navigate = useNavigate();
-
-    const blocks = [];
-
+    
+    const [blocks, setBlocks] = useState([]);
+    const [renderCount, setRenderCount] = useState(0);
+    const renderRef = useRef(false);
+    
     const x = window.innerWidth;
     const y = window.innerHeight;
     const blockSideLength = 50;
-    const blockCount = ((x/blockSideLength) * (y/blockSideLength)) * 5;
+    const blockCount = Math.floor((x/blockSideLength) * (y/blockSideLength)) * 4;
     
-    for(let i = 0 ; i < blockCount ; i++){
-        const hexCode = generateHex();
-        blocks.push(<ColorBlock hex={hexCode} clickHandler = {() => navigate(`/palette/${hexCode}`)} />);
+    useEffect(() => {
+        function renderBlocks(){
+            const tempBlocks = [];
+            for(let i = 0 ; i < blockCount ; i++){
+                const hexCode = generateHex();
+                tempBlocks.push(<ColorBlock key={((renderCount*blockCount)+i+Date.now().toString())} hex={hexCode} clickHandler = {() => navigate(`/palette/${hexCode}`)} />);
+            }
+            setBlocks(prevBlocks => {
+                return [...prevBlocks, ...tempBlocks];
+            });
+            return tempBlocks;
+        }
+        renderBlocks();
+    }, [renderCount, blockCount, navigate]);
+
+    const handleScroll:UIEventHandler<HTMLElement> = (e) => {
+        const element = e.currentTarget;
+        if(element.scrollTop > element.scrollHeight * (1 - (1/((renderCount || 1) + 1))/2) && !renderRef.current){
+            setRenderCount(() => renderCount+1);
+
+            renderRef.current = true;
+
+            setTimeout(() => {
+                renderRef.current = false;
+            }, 300);
+        }
     }
     
     return (
-        <main className="content__grid">
+        <main className="content__grid" onScroll={handleScroll}>
             {blocks}
         </main>
     )
