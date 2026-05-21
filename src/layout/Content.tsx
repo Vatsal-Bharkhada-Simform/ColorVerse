9ae225d3ba748b8d7f0819c4353a145a4dc2c5de
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-05-21 12:34:13 +0530

    Chore: Add comments.

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -8,31 +8,39 @@ export default function Content(){
     
     const [blocks, setBlocks] = useState<ReactElement[]>([]);
     const [renderCount, setRenderCount] = useState(0);
+
+    // Ref to store boolean flag across re-renders
     const renderRef = useRef(false);
     
     const x = window.innerWidth;
     const y = window.innerHeight;
     const blockSideLength = 50;
+
+    // Calculate the number of blocks needed to create a carousel having four times the viewport height.
     const blockCount = Math.floor((x/blockSideLength) * (y/blockSideLength)) * 4;
     
+    // Initial render
+    // Add new color blocks to the carousel if "renderCount" changes.
     useEffect(() => {
         function renderBlocks(){
             const tempBlocks: ReactElement[] = [];
             for(let i = 0 ; i < blockCount ; i++){
-                const hexCode = generateHex();
+                const hexCode = generateHex();              // Generate a unique hash(color)
                 tempBlocks.push(<ColorBlock key={((renderCount*blockCount)+i+Date.now().toString())} hex={hexCode} clickHandler = {() => navigate(`/palette/${hexCode}`)} />);
             }
             setBlocks(prevBlocks => {
-                return [...prevBlocks, ...tempBlocks];
+                return [...prevBlocks, ...tempBlocks];      // Append new blocks to the previous ones
             });
             return tempBlocks;
         }
         renderBlocks();
     }, [renderCount, blockCount, navigate]);
 
+    // Handler to manage infinite scrolling
     const handleScroll:UIEventHandler<HTMLElement> = (e) => {
         const element = e.currentTarget;
-        if(element.scrollTop > element.scrollHeight * (1 - (1/((renderCount || 1) + 1))/2) && !renderRef.current){
+        // If the scrolled content exceeds a calculated threshold, add new blocks
+        if(element.scrollTop > element.scrollHeight * (1 - (1/((renderCount || 1)*2 + 1))) && !renderRef.current){
             setRenderCount(() => renderCount+1);
 
             renderRef.current = true;
--- src/utils/generateHex.ts
+++ src/utils/generateHex.ts
@@ -1,8 +1,8 @@
-export function generateHex(): string{
-    return Array(6).fill(0).reduce((prev: string) => {
-        prev = prev + Math.floor(Math.random() * 16).toString(16);
-        return prev;
-    }, "#");
-}
+// Function to generate random six-digit hex codes which will be used as color codes
+// "padStart" assures that the generated output has 6 characters and add a padding if not. 
+// "16777215" is the maximum decimal value a six-digit hex number can have.
+// Hence we generate a random number in this range and convert it to hex code to get the color.
 
-console.log(generateHex())
+export function generateHex(): string{
+    return "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, 'F');
+}
